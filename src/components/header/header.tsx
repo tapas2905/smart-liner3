c5c17c495f61eb3a4a7d441e445f341302eaa439
tapasmandalOgmaConceptions <tapas.mandal@ogmaconceptions.com>
2025-06-19 08:50:33 +0530

    changes in header

--- src/components/header/header.tsx
+++ src/components/header/header.tsx
@@ -2,16 +2,19 @@ import React from 'react';
 import { useState, useRef, useEffect } from 'react';
 import { Link } from 'react-router-dom';
 import styles from './header.module.scss';
-import { useDispatch } from "react-redux";
-import { AppDispatch } from "../../store";
+import { useDispatch, useSelector } from "react-redux";
+import { AppDispatch, RootState } from "../../store";
 import { setLogout } from "../../store/userSlice";
 import alert from "../../services/alert";
 
 const Header: React.FC  = () => {
   const [isProfileOpen, setIsProfileOpen] = useState(false);
   const dropdownRef = useRef(null);
-   const dispatch: AppDispatch = useDispatch();
-
+  const dispatch: AppDispatch = useDispatch();
+  const isAuthenticated = useSelector(
+    (state: RootState) => state.user.isAuthenticated
+  );
+  const userInfo = useSelector((state: RootState) => state.user.userInfo);
   const toggleProfile = () => {
     setIsProfileOpen(!isProfileOpen);
   };
@@ -50,14 +53,15 @@ const Header: React.FC  = () => {
                 <Link to={'#'}>Home</Link>
               </li>
               <li>
-                <Link to={'#'}>Product List</Link>
+                <Link to={'/'}>Product List</Link>
               </li>
               <li>
                 <Link to={'#'}>Shop Online</Link>
               </li>
             </ul>
           </div>
-          <div
+          { isAuthenticated ? (
+            <div
             className={styles.hdrSignInPrt}
             onClick={toggleProfile}
             ref={dropdownRef}
@@ -65,14 +69,14 @@ const Header: React.FC  = () => {
             <div className={styles.hdrSignBtn}>
               <Link to={'#'}>
                 <i className="fa-regular fa-circle-user"></i>
-                <span>Sign In</span>
+                <span>{userInfo?.name}</span>
               </Link>
             </div>
             {isProfileOpen && (
               <div className={styles.hdrProfileDropdown}>
                 <ul>
                   <li>
-                    <Link to="#">
+                    <Link to="/my-profile">
                       <i className="fa-regular fa-circle-user"></i>
                       <span>Profile</span>
                     </Link></li>
@@ -86,6 +90,18 @@ const Header: React.FC  = () => {
               </div>
             )}
           </div>
+          ) : (
+            <div
+            className={styles.hdrSignInPrt}
+          >
+            <div className={styles.hdrSignBtn}>
+              <Link to={'/login'}>
+                <i className="fa-regular fa-circle-user"></i>
+                <span>Sign In</span>
+              </Link>
+            </div>
+          </div>
+          )}
         </div>
       </div>
     </div>
